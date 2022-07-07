ea771026854e2e2e1b808bc80934779becbbc8d1
Aryan Rishi Lamba <aryanrishi20@gmail.com>
2022-07-07 19:30:19 +0530

    Bear put spread

---
+++ bear-spread-call.py
@@ -0,0 +1,27 @@
+# Function to find the put payoff
+def put_payoff(strike_price, premium, underlying_price):
+    return max(strike_price - underlying_price, 0) - premium
+
+# Stock Price
+underlying_price = 200
+
+# placing a long put
+strike_price_LP = 220
+premium_LP = 10
+
+# Placing a short put
+strike_price_SP = 180
+premium_SP = 8
+
+# Payoff from the long put
+LP_payoff =  put_payoff(strike_price_LP, premium_LP, underlying_price)
+
+# Payoff from the short put
+SP_payoff =  put_payoff(strike_price_SP, premium_SP, underlying_price)
+
+# Payoff from the bear spread
+bear_spread = LP_payoff + SP_payoff
+
+print("Payoff from the long put: ", LP_payoff)
+print("Payoff from the short put: ", SP_payoff)
+print("Payoff from the bear spread: ", bear_spread)
